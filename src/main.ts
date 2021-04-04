1043667c8ea39736f000116e066886db05ad09e5
wasabi315 <wasabi.nvim.st@gmail.com>
2021-04-04 22:30:06 +0900

    Capture stdout

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,42 @@ import { prettyprint } from '~/ast/prettyprint';
 import { createInterpreter } from '~/interpreter';
 import { Int, traceInt, fact } from '~/programs';
 
+const captureStdout = (fn: () => unknown): string => {
+  const origWrite = process.stdout.write.bind(process.stdout);
+
+  let out = '';
+  process.stdout.write = ((chunk: string | Uint8Array) => {
+    if (typeof chunk === 'string') {
+      out += chunk;
+    }
+  }) as typeof origWrite;
+  fn();
+  process.stdout.write = origWrite;
+
+  return out;
+};
+
 const run = (program: ast.Program): void => {
   console.group('Program');
   console.log(prettyprint(program));
   console.groupEnd();
   console.log();
 
-  console.group('Result');
   const interpreter = createInterpreter(program);
+  let out: string;
   try {
     // Run interpreter
-    for (const _ of interpreter);
-  } catch (err: unknown) {
-    console.error(
-      'Program exited with error: ',
+    out = captureStdout(() => {
+      for (const _ of interpreter);
+    });
+  } catch (err) {
+    out = `Program exited with error: ${
       err instanceof Error ? err.message : err
-    );
+    }`;
   }
+  console.log('Result');
+  console.group();
+  console.log(out);
   console.groupEnd();
 };
 
