c4752b6f0dfb92aba921019e84998a3c8ca205c2
wasabi315 <wasabi.nvim.st@gmail.com>
2021-06-08 10:34:49 +0900

    Refactor

--- src/ast/prettyprint.ts
+++ src/ast/prettyprint.ts
@@ -8,55 +8,54 @@ export const prettyprint = (program: ast.Program): string => {
 const TAB_SIZE = 4;
 
 const ppBinds = (binds: ast.Binds): pp.IDoc => {
-  return pp.intersperse(
-    pp.prepend(pp.line, pp.line),
-    [...Object.entries(binds)].map((bind) => ppBind(...bind))
+  return ppUnlines2(
+    ...[...Object.entries(binds)].map((bind) => ppBind(...bind))
   );
 };
 
 const ppBind = (v: ast.Var, lf: ast.LambdaForm): pp.IDoc => {
-  return pp.intersperse(' ', [v, '=', ppLambdaForm(lf)]);
+  return ppUnwords(v, '=', ppLambdaForm(lf));
 };
 
 const ppLambdaForm = (lf: ast.LambdaForm): pp.IDoc => {
-  return pp.intersperse(pp.line, [
-    pp.intersperse(' ', [
+  return ppUnlines(
+    ppUnwords(
       ppVars(lf.free),
       lf.updatable ? '\\u' : '\\n',
       ppVars(lf.args),
-      '->',
-    ]),
-    pp.indent(TAB_SIZE, ppExpr(lf.expr)),
-  ]);
+      '->'
+    ),
+    pp.indent(TAB_SIZE, ppExpr(lf.expr))
+  );
 };
 
 const ppExpr = (expr: ast.Expr): pp.IDoc => {
   if (ast.isLet(expr)) {
-    return pp.intersperse(pp.line, [
+    return ppUnlines(
       expr.rec ? 'letrec' : 'let',
       pp.indent(TAB_SIZE, ppBinds(expr.binds)),
       'in',
-      pp.indent(TAB_SIZE, ppExpr(expr.expr)),
-    ]);
+      pp.indent(TAB_SIZE, ppExpr(expr.expr))
+    );
   }
 
   if (ast.isCase(expr)) {
-    return pp.intersperse(pp.line, [
-      pp.intersperse(' ', ['case', ppExpr(expr.expr), 'of']),
-      pp.indent(TAB_SIZE, ppAlts(expr.alts)),
-    ]);
+    return ppUnlines(
+      ppUnwords('case', ppExpr(expr.expr), 'of'),
+      pp.indent(TAB_SIZE, ppAlts(expr.alts))
+    );
   }
 
   if (ast.isVarApp(expr)) {
-    return pp.intersperse(' ', [expr.var, ppAtoms(expr.args)]);
+    return ppUnwords(expr.var, ppAtoms(expr.args));
   }
 
   if (ast.isConstrApp(expr)) {
-    return pp.intersperse(' ', [expr.constr, ppAtoms(expr.args)]);
+    return ppUnwords(expr.constr, ppAtoms(expr.args));
   }
 
   if (ast.isPrimApp(expr)) {
-    return pp.intersperse(' ', [expr.prim, ppAtoms(expr.args)]);
+    return ppUnwords(expr.prim, ppAtoms(expr.args));
   }
 
   if (ast.isLiteral(expr)) {
@@ -67,14 +66,14 @@ const ppExpr = (expr: ast.Expr): pp.IDoc => {
 };
 
 const ppAlts = (alts: ast.Alt[]): pp.IDoc => {
-  return pp.intersperse(pp.prepend(pp.line, pp.line), alts.map(ppAlt));
+  return ppUnlines2(...alts.map(ppAlt));
 };
 
 const ppAlt = (alt: ast.Alt): pp.IDoc => {
   let pattern: pp.IDoc;
 
   if (ast.isAlgAlt(alt)) {
-    pattern = pp.intersperse(' ', [alt.constr, ppVars(alt.vars)]);
+    pattern = ppUnwords(alt.constr, ppVars(alt.vars));
   } else if (ast.isPrimAlt(alt)) {
     pattern = ppLiteral(alt.literal);
   } else if (ast.isVarAlt(alt)) {
@@ -83,21 +82,19 @@ const ppAlt = (alt: ast.Alt): pp.IDoc => {
     pattern = 'default';
   }
 
-  return pp.intersperse(pp.line, [
-    pp.intersperse(' ', [pattern, '->']),
-    pp.indent(TAB_SIZE, ppExpr(alt.expr)),
-  ]);
+  return ppUnlines(
+    ppUnwords(pattern, '->'),
+    pp.indent(TAB_SIZE, ppExpr(alt.expr))
+  );
 };
 
 const ppVars = (vs: ast.Var[]): pp.IDoc => {
-  return pp.enclose(pp.braces, pp.intersperse(',', vs));
+  return ppBraces(ppSepByComma(...vs));
 };
 
 const ppAtoms = (atoms: ast.Atom[]): pp.IDoc => {
-  return pp.enclose(
-    pp.braces,
-    pp.intersperse(
-      ',',
+  return ppBraces(
+    ppSepByComma(
       atoms.map((atom) => {
         return ast.isLiteral(atom) ? ppLiteral(atom) : atom;
       })
@@ -107,4 +104,25 @@ const ppAtoms = (atoms: ast.Atom[]): pp.IDoc => {
 
 const ppLiteral = (literal: ast.Literal): pp.IDoc => {
   return `${literal}#`;
+};
+
+// Utils
+const ppUnwords = (...docs: pp.IDoc[]): pp.IDoc => {
+  return pp.intersperse(' ', docs);
+};
+
+const ppUnlines = (...docs: pp.IDoc[]): pp.IDoc => {
+  return pp.intersperse(pp.line, docs);
+};
+
+const ppUnlines2 = (...docs: pp.IDoc[]): pp.IDoc => {
+  return pp.intersperse(pp.prepend(pp.line, pp.line), docs);
+};
+
+const ppSepByComma = (...docs: pp.IDoc[]): pp.IDoc => {
+  return pp.intersperse(',', docs);
+};
+
+const ppBraces = (doc: pp.IDoc): pp.IDoc => {
+  return pp.enclose(pp.braces, doc);
 };
